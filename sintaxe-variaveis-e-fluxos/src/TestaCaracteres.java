9a0791751adeadeffec41e5e32af0e55e364b45c
Flavio Lopes da Silva <flaviolopes@mtprev.local>
2021-06-21 03:46:43 -0400

    Alura Inicio

---
+++ sintaxe-variaveis-e-fluxos/src/TestaCaracteres.java
@@ -0,0 +1,23 @@
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		//casting de int para char
+		valor = (char) (valor + 1);
+		System.out.println(valor);
+		
+		String palavra = "Frase completa de texto";
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxos/src/TestaConversao.java
@@ -0,0 +1,18 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		//casting - convertendo double em int
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double resultado = valor1 + valor2;
+		System.out.println(resultado);
+		
+
+	}
+
+}
