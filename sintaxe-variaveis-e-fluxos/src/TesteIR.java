061140001f26c05ea5cc38e660950f84ad163947
Flavio Lopes da Silva <flaviolopes@mtprev.local>
2021-06-22 02:59:21 -0400

    Estudo de Escopos, Fluxos e Boolean

---
+++ sintaxe-variaveis-e-fluxos/src/TesteIR.java
@@ -0,0 +1,29 @@
+
+public class TesteIR {
+
+	public static void main(String[] args) {
+
+		/*
+		 * João gostaria de criar um programa sobre Imposto de Renda (IR) e 
+		 * verificou as regras de alíquota. Ele descobriu no site da receita:
+		 * 
+		 * De 1900.0 até 2800.0, o IR é de 7.5% e pode deduzir na declaração 
+		 * o valor de R$ 142 De 2800.01 até 3751.0, o IR é de 15% e pode 
+		 * deduzir R$ 350 De 3751.01 até 4664.00, o IR é de 22.5% e pode 
+		 * deduzir R$ 636 
+		 */
+		double salario = 3300.0;
+		
+		if (salario >= 1900.0 && salario <= 2800.0) {
+			System.out.println("Pode deduzir na declaração o valor de R$ 142");			
+		} else if (salario > 2800.0 && salario <= 3751.0) {
+			System.out.println("Pode deduzir R$ 350");
+		} else if (salario > 3751.0 && salario <= 4664.0) {
+			System.out.println("Pode dedusir R$ 636.");
+		} else {
+			System.out.println("Regra não encontrada.");
+		}
+
+	}
+
+}
