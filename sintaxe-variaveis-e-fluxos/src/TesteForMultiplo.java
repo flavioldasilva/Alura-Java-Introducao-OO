289cbac0a4495742e256d6f916782bfc5570135b
flavio lopes silva <digitalblu@gmail.com>
2021-06-25 10:22:47 -0400

    Corrigindo Projeto

---
+++ sintaxe-variaveis-e-fluxos/src/TesteForMultiplo.java
@@ -0,0 +1,10 @@
+public class TesteForMultiplo {
+	public static void main(String[] args) {
+		int numero = 100;
+		for (int contador = 0; contador < 100; contador++) {
+			if (numero % 3 == 0) {
+				System.out.println(numero);
+			}
+		}
+	}
+}
