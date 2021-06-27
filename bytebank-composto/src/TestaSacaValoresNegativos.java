4f9ced64b0412da4ffd7c17c9fb7a98f0c077756
Flavio Lopes da Silva <digitalblu@gmail.com>
2021-06-27 06:39:22 -0400

    Trabalhando com Classes Compostas

---
+++ bytebank-composto/src/TestaSacaValoresNegativos.java
@@ -0,0 +1,20 @@
+
+public class TestaSacaValoresNegativos {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.deposita(100);
+		System.out.println(conta.saca(101));
+		
+		conta.saca(101);
+		
+		System.out.println(conta.pegaSaldo());
+		
+		/*nao pode acontecer
+		 * conta.saldo = conta.saldo - 101; System.out.println(conta.saldo);
+		 * 
+		 * conta.saca(200); System.out.println(conta.saldo);
+		 */
+		
+	}
+
+}
